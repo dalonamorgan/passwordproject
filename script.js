df760ec31e8422565bc15a2cacc88ef457183277
TCP Student 12 <student12@techbridge.org>
2021-06-25 21:20:06 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Set static variables
+  
+var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var abcUpperArr = abcUpper.split("");
+var abcLower = "abcdefghijklmnopqrstuvwxyz";
+var abcLowerArr = abcLower.split("");
+var num = "0123456789";
+var numArr = num.split("");
+var sym = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+var symArr = sym.split("");
